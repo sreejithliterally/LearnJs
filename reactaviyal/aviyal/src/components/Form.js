0574312f4f4a27c92202ca5519629f0db4a1c476
sreejithliterally <imsreejithofficial@gmail.com>
2022-08-27 23:33:19 +0530

    fix

--- reactaviyal/aviyal/src/components/Form.js
+++ reactaviyal/aviyal/src/components/Form.js
@@ -2,22 +2,29 @@ import React, { Component } from 'react'
 
 class Form extends Component {
 
+    state = {
+        inputValue:"trtrtr"
+
+    }
+
     onHandleChange=(event)=>{
         this.setState({
             inputValue: event.target.value
-        })
-;    }
-    state = {
-        inputValue:""
-
+        });
     }
+    
+
+    onSubmit =()=>{
+        console.log(this.state)
+    }
+  
     render() {
         return (
                 <form>
                 
                 <h2> Form component</h2>
                 <input type ="text" value={this.setState.inputValue} onChange={this.onHandleChange}></input>
-
+                <button type="button" onClick={this.onSubmit}>submit</button>
                 </form>
         )
     }
